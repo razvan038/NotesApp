8d43383936d77dd5b121a065e1ba722731bfc8d2
Razvan Stefan <razvanionut038@gmail.com>
2024-03-26 12:33:51 +0200

    Fixed the View Note function!

---
+++ server/controllers/mainController.js
@@ -0,0 +1,30 @@
+/** 
+ * GET/
+ * Home Page
+ * */ 
+
+exports.homepage = async (req, res) => {
+        const locals = {
+            title: 'NodeJs Notes',
+            description: 'Free NodeJs Notes App'
+        }
+    
+        res.render('index', {
+            locals,
+            layout: '../views/layouts/front-page'
+        });
+    }
+
+    /** 
+ * GET/
+ * About
+ * */ 
+
+exports.about = async (req, res) => {
+    const locals = {
+        title: 'About - NodeJs Notes',
+        description: 'Free NodeJs Notes App'
+    }
+
+    res.render('about', locals);
+}
